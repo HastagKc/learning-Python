0a9a1ae4ca6bc05f2b8fd518e9b62fee0bce2b12
HastagKc <chaudharykshittiz950@gmail.com>
2024-02-05 13:16:03 +0545

    Numbers

--- 01_basics/02_number.py
+++ 01_basics/02_number.py
@@ -32,3 +32,54 @@ print(modulus_result)
 
 exponentation_result = 2**3
 print(exponentation_result)
+
+
+# type conversion 
+int_value = 22 
+int_to_float = float(int_value)
+print(int_to_float) # 22.0
+
+float_value = 25.075
+float_to_int = int(float_value) # decimal part get remove 
+print(float_to_int) # 25
+
+
+
+# int and float to complex 
+int_num = 22
+int_to_complex = complex(int_num)
+print("Int to complex :", int_to_complex) # 22+0j 
+
+
+float_num = 22.05
+float_to_complex = complex(float_num)
+print("Float to complex : ", float_to_complex)
+
+'''
+
+When converting a complex number to an integer or a float in Python, 
+you usually extract the real or imaginary part depending on your needs.
+Here's an example demonstrating both conversions:
+
+'''
+
+
+complex_value = 2+3j
+complex_to_float = float(complex_value.imag)
+print(complex_to_float)
+
+
+complex_to_int = int(complex_value.real)
+print(complex_to_int)
+
+
+# Built-in Functions
+abs_result = abs(-5)
+rounded_num = round(3.54149, 3)
+max_value = max(4, 8, 2, 200)
+min_value = min(4, 8, 2, -10)
+
+print("abs_result: ", abs_result)
+print("rounded_num: ",rounded_num)
+print("max_value: ",max_value)
+print("min_value: ",min_value)
