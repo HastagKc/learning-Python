23c4623a51fe7ad7bb920c14c6657580c61ae02f
HastagKc <chaudharykshittiz950@gmail.com>
2024-02-05 11:17:04 +0545

    Data types and variable

---
+++ 01_basics/01_variable.py
@@ -0,0 +1,18 @@
+# variable and datatypes
+# variable is a container to store value
+# it is a named memory location where value is store
+
+# String variable
+person_name = "kshittiz"
+print(person_name)
+
+# Integer variable
+person_age = 22
+print(person_age)
+
+# Boolean
+person_marriage_status = False
+
+# float
+person_height = 5.6
+print(person_height)
