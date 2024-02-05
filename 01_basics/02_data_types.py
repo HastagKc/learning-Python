d9fdfe79a41d225ddaf0257ec5baa8624e7c0a25
HastagKc <chaudharykshittiz950@gmail.com>
2024-02-05 12:36:45 +0545

    Number

---
+++ 01_basics/02_data_types.py
@@ -0,0 +1,27 @@
+# Data types define the kind of values that variables can
+# hold and the operations that can be performed on them
+
+# Numeric data types
+integer_number = 42
+float_number = 3.14
+complex_number = 2 + 3j
+
+# Text data type
+text_string = "Hello, Python!"
+
+# Boolean data type
+is_true = True
+is_person = False
+
+# Sequence data types
+list_data = [1, 2, 3]
+tuple_data = (4, 5, 6)
+
+# Set data type
+set_data = {1, 2, 3}
+
+# Mapping data type
+dictionary_data = {"name": "John", "age": 25}
+
+# NoneType
+null_value = None
---
+++ 01_basics/02_number.py
@@ -0,0 +1,34 @@
+# Number types in python 
+# integer, float and complex types 
+
+# int
+int_type = 22
+
+# float 
+float_type = 22.000
+
+# complex type
+complex_type = 3+5j 
+
+print(type (int_type)) # <class 'int'>
+print(type(float_type)) # float type 
+print(type(complex_type)) # complex type 
+
+# mathmatical operation 
+addition_result = 22 + 22.05
+print(addition_result)
+
+subtraction_result = 75 - 22.5
+print(subtraction_result) 
+
+multiplication_result = 10 * 22
+print(multiplication_result) 
+
+division_result = 22 / 2
+print(division_result)
+
+modulus_result = 22 % 3 
+print(modulus_result)
+
+exponentation_result = 2**3
+print(exponentation_result)
