d53ee0e9c7b684292da83dc458f2aa78eafeef68
HastagKc <chaudharykshittiz950@gmail.com>
2024-02-07 19:02:26 +0545

    String in js

---
+++ 01_basics/04_input_output.py
@@ -0,0 +1,15 @@
+# input function is used to take input from user 
+# in python 
+
+user_name = input("Enter your name: ")
+print(user_name.capitalize())
+
+# basic calculation 
+# input return string so we have to type cast 
+num1 = input("Enter First Number: ")
+num2 = input("Enter Second Number: ")
+
+result =int(num1) + int(num2) 
+print(result)
+
+
---
+++ 01_basics/05_string.py
@@ -0,0 +1,58 @@
+# Strings in Python are sequences of characters, enclosed within either single quotes
+# (') or double quotes ("). They are immutable, meaning once defined, their contents cannot be 
+# changed. Strings support various operations, including concatenation, slicing, formatting, and
+# more.
+
+# declaration and initizition of string 
+message = "Namaste from Nepal."
+discription = "Nepal is beautiful country"
+
+# concatenation 
+full_message = message+ " "+ discription
+print(full_message)
+
+
+# String Indexing and Slicing:
+user_name = "kshittiz"
+print(user_name[0]) # k
+print(user_name[-1]) # z
+
+print(user_name[:5]) # kshit
+print(user_name[1:]) # shittiz
+print(user_name[1:5]) # shit index 5 will not include 
+print(user_name[::2]) # print all laternative words 
+
+
+
+# String Length
+dis = "I like mountain bikes"
+print(len(dis))
+
+# string method 
+message = "Hello, World!"
+uppercase = message.upper()           # "HELLO, WORLD!"
+lowercase = message.lower()           # "hello, world!"
+capitalized = message.capitalize()    # "Hello, world!"
+replaced = message.replace("World", "Python")   # "Hello, Python!"
+
+# string formating 
+name = "Kshittiz"
+age = 22
+formatted_string = f"My name is {name} and I'm {age} years old."
+print(formatted_string)
+
+num1 = 22
+num2 = 23 
+result=f"Sum of {num1} + {num2} = {num1 + num2}"
+print(result)
+
+result2 = "The sum of {} + {} = {}".format(num1, num2, num1 + num2)
+print(result2)
+
+# Escape Characters:
+escaped_string = "This is a newline \nand this is a tab \tcharacter."
+print(escaped_string)
+
+
+
+
