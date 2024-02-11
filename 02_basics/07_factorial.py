9dc4e3444354ebbb3d618d71d1bab02930866526
HastagKc <chaudharykshittiz950@gmail.com>
2024-02-11 18:58:03 +0545

    list in python

---
+++ 02_basics/07_factorial.py
@@ -0,0 +1,6 @@
+# 7. Program to calculate the factorial of a number using recursion.
+# Formula to find factorial n! = n * (n-1) * _________ * 1
+num = 5
+for i in range(1, num):
+    num = num * i
+print(num)
---
+++ 03_basics/01_list.py
@@ -0,0 +1,6 @@
+
+'''
+In Python, a list is a collection of items enclosed within square brackets [ ]. Lists are ordered and mutable, meaning you can change, add, and remove elements after the list is created. Lists can contain elements of different data types, including integers, floats, strings, and even other lists. 
+
+'''
+
