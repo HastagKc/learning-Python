6b255ff851931339e5bd9a84967c832941b5969a
HastagKc <chaudharykshittiz950@gmail.com>
2024-02-13 23:20:27 +0545

    changing file names

--- 06_oop/01_exercise01.py
+++ 06_oop/01_exercise01.py
@@ -1,38 +1,6 @@
 # 1. Bank Account Management System:
 # -> Create a class representing a bank account with methods to deposit, withdraw, and display balance.
 
-# class BankAccount:
-#     def __init__(self, account_number, account_holder, total_balance=0):
-#         self.account_number = account_number
-#         self.account_holder = account_holder
-#         self.balance = total_balance
-
-#     def deposit(self, amount):
-#         if amount > 0:
-#             self.balance += amount
-#             print(f"Deposited {amount} successfully.")
-#         else:
-#             print("Invalid deposit amount. Please enter a positive number.")
-
-#     def withdraw(self, amount):
-#         if amount > 0 and amount <= self.balance:
-#             self.balance -= amount
-#             print(f"Withdrawn {amount} successfully.")
-#         else:
-#             print("Invalid withdrawal amount. Please check your balance and try again.")
-
-#     def display_balance(self):
-#         print(f"Account Holder: {self.account_holder}")
-#         print(f"Account Number: {self.account_number}")
-#         print(f"Current Balance: {self.balance}")
-
-# # Example usage:
-# account1 = BankAccount("123456789", "Alice")
-# account1.deposit(1000)
-# account1.withdraw(500)
-# account1.display_balance()
-
-
 # requirement account no 
 # account holder 
 # initial balance 
