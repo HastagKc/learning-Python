f72cb13931180ccefd7a700721416158115125a3
HastagKc <chaudharykshittiz950@gmail.com>
2024-02-21 09:39:11 +0545

    todo add functinality

--- 07_file_handling.py/01_basic.py
+++ 07_file_handling.py/01_basic.py
@@ -1,8 +1,10 @@
 # opening file 
 file = open('E:/#LearningPython/07_file_handling.py/01_basic.txt', 'r')
+# print(file)
 
 # Reading from a file 
 content = file.read()
+print(content)
 
 # write to a file 
 # to use write method we should open with write mode 
