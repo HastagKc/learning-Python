11049adead321caf1938ccd8c7b37cc7cd924b89
HastagKc <chaudharykshittiz950@gmail.com>
2024-03-03 08:56:53 +0545

    ecommerce

---
+++ 07_file_handling.py/ecommerce/ecommerce_01.py
@@ -0,0 +1,137 @@
+# Requirements
+# Ask user to login or register
+# If register ask for username,password and usertype and store the data in a file the format of storing the user data can change because of extra field usertype(buyer/seller)
+# If login ask for username and password and check if it matches with any data, if it matches then check the usertype of the data and if the type is buyer give hime option of view product, purchase product,view bill. If the type is seller give him option of add product, view his/her product, view bills
+# If the seller user provides option for add product, ask him/her for product name, description, price and the store this data along with user as the seller key value as a json in a file
+# If the buyer user provides option for view product, print all the product from the file where product data is stored
+
+
+import json
+
+
+
+
+
+def add_product(seller):
+    product_name = input('Enter the product name : ')
+    product_price = input('Enter the product price : ')
+    product_description = input('Enter the product description : ')
+
+    product_dict_data = {'name':product_name,'description':product_description,'price':product_price,'seller':seller}
+
+    json_product_data = json.dumps(product_dict_data)
+
+    f = open('E:/#LearningPython/07_file_handling.py/ecommerce/product.txt','a')
+
+    f.write(json_product_data+'-')
+
+    f.close()
+
+def view_your_product(seller):
+    f = open('E:/#LearningPython/07_file_handling.py/ecommerce/product.txt','r')
+    product_data = f.read()
+    f.close()
+
+    list_product_data = product_data.split('-')
+
+    for product in list_product_data:
+        if product != '':
+            product_dict_data = json.loads(product)
+            if product_dict_data.get('seller') == seller:
+                print(product_dict_data)
+
+
+
+def view_product():
+    f = open('D:/Work/MindrisersTeaching/Magh/product.txt','r')
+
+    product_data = f.read()
+
+    f.close()
+
+    list_product_data = product_data.split('-')
+
+    for product in list_product_data:
+        print(product)
+
+def register():
+    username = input('Enter your username : ')
+    password = input('Enter your password : ')
+    usertype = input('Enter your usertype(buyer/seller) : ').lower()
+
+    user_data = {"username":username,"password":password,"usertype":usertype}
+
+    f = open('E:/#LearningPython/07_file_handling.py/ecommerce/userdata.txt','a')
+
+    json_user_data = json.dumps(user_data) # To convert dictionary data to json data
+
+    f.write(json_user_data + '-') # Writing json data in the file
+
+    f.close()
+    print('Register Sucessfully')
+
+def login():
+    username = input('Enter your username : ')
+    password = input('Enter your password : ')
+
+    f = open('E:/#LearningPython/07_file_handling.py/ecommerce/userdata.txt','r')
+
+    user_datas = f.read()
+
+    f.close()
+
+    list_user_datas = user_datas.split('-')
+    user_login = False
+    usertype = None
+    for i in list_user_datas:
+        if i != '':
+            dict_data = json.loads(i)
+            if username == dict_data.get('username') and dict_data.get('password') == password:
+                user_login = True
+                usertype = dict_data.get('usertype')
+
+
+    if user_login == True:
+        print('Login successfull!')
+
+
+        if usertype == 'buyer':
+            print('''
+                    Enter the option from below :
+                    1. View product
+                    2. Purchase product
+                    3. View bill''')
+            user_option_choice = input()
+            if user_option_choice == '1':
+                view_product()
+
+            elif user_option_choice == '2':
+                pass
+            elif user_option_choice == '3':
+                pass
+            else:
+                print('Invalid options, please choose correct option')
+                login()
+
+        elif usertype == 'seller':
+            print('''
+                    Enter the option from below :
+                    1. Add product
+                    2. View your product
+                    3. View your product bill''')
+            user_option_choice = input()
+            if user_option_choice == '1':
+                add_product(username)
+            elif user_option_choice == '2':
+                view_your_product(username)
+
+    else:
+        print('Invalid credentials')
+
+
+user_choice = input('Do you want to login or register? ').lower().strip()
+
+if user_choice == 'register':
+    register()
+elif user_choice == 'login':
+    login()
