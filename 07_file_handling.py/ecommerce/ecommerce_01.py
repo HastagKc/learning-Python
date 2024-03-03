e5586d48cc5747ed93669da91f80d31edfb0a2c8
HastagKc <chaudharykshittiz950@gmail.com>
2024-03-03 17:39:56 +0545

    bill of customer

--- 07_file_handling.py/ecommerce/ecommerce_01.py
+++ 07_file_handling.py/ecommerce/ecommerce_01.py
@@ -8,8 +8,91 @@
 
 import json
 
+def purchaseProduct(usertype,username):
+    if usertype == 'buyer':
+        f = open('E:/#LearningPython/07_file_handling.py/ecommerce/product.txt','r')
+        product_data = f.read()
+        list_product_data = product_data.split('-')
+        print("Available Products:")
+        for i, product in enumerate(list_product_data, start=1):
+            if product:
+                product_dict_data = json.loads(product)
+                print(f"{i}. {product_dict_data['name']} - ${product_dict_data['price']}")
+
+        choice = int(input("Enter the number of the product you want to purchase: ")) - 1
+        if 0 <= choice < len(list_product_data):
+            # converting into dict
+            selected_product = json.loads(list_product_data[choice])
+            selected_product['buyer'] = username
+            print(f"You have purchased: {selected_product['name']} for ${selected_product['price']}")
+            f = open('E:/#LearningPython/07_file_handling.py/ecommerce/purchase_product.txt','a')
+            # converting dict to string 
+            seleted_product_in_string = json.dumps(selected_product)
+            f.write(seleted_product_in_string +'-')
+            f.close()
 
 
+
+def view_bill(username):
+    total = 0.0
+    f = open('E:/#LearningPython/07_file_handling.py/ecommerce/purchase_product.txt','r')
+    purchase_datas = f.read()
+    f.close()
+    list_purchase_datas = purchase_datas.split('-')
+
+    for data in list_purchase_datas:
+        if data.strip():
+            dict_data = json.loads(data)
+            if username == dict_data.get('buyer'):
+                print(f'Product name: {dict_data.get('name')}')
+                print(f'Product price: {dict_data.get('price')}')
+                total = total + float(dict_data.get('price'))
+    print(f'Total: {total}')
+                
+      
+def usertypeFun(usertype, username):
+    if usertype == 'buyer':
+        print('''
+            Enter the option from below :
+            1. View product
+            2. Purchase product
+            3. View bill
+            ''')
+        user_option_choice = input()
+        if user_option_choice == '1':
+            view_product()
+
+        elif user_option_choice == '2':
+            purchaseProduct(usertype,username)
+        elif user_option_choice == '3':
+            view_bill(username)
+
+        else:
+            print('Invalid options, please choose correct option')
+            usertypeFun(usertype,username)
+        
+
+    elif usertype == 'seller':
+        print('''
+            Enter the option from below :
+            1. Add product
+            2. View your product
+            3. View your product bill''')
+        user_option_choice = input()
+        if user_option_choice == '1':
+            add_product(username)
+        elif user_option_choice == '2':
+            view_your_product(username)
+        elif user_option_choice == '2':
+            view_your_product(username)
+
+    else:
+        print('''
+              Invalid usertypes
+              Please Login again
+              ''')
+
+        login()
 
 
 def add_product(seller):
@@ -23,7 +106,7 @@ def add_product(seller):
 
     f = open('E:/#LearningPython/07_file_handling.py/ecommerce/product.txt','a')
 
-    f.write(json_product_data+'-')
+    f.write(json_product_data +'-')
 
     f.close()
 
@@ -43,7 +126,7 @@ def view_your_product(seller):
 
 
 def view_product():
-    f = open('D:/Work/MindrisersTeaching/Magh/product.txt','r')
+    f = open('E:/#LearningPython/07_file_handling.py/ecommerce/product.txt','r')
 
     product_data = f.read()
 
@@ -53,6 +136,7 @@ def view_product():
 
     for product in list_product_data:
         print(product)
+
 
 def register():
     username = input('Enter your username : ')
@@ -90,48 +174,23 @@ def login():
                 user_login = True
                 usertype = dict_data.get('usertype')
 
-
     if user_login == True:
         print('Login successfull!')
-
-
-        if usertype == 'buyer':
-            print('''
-                    Enter the option from below :
-                    1. View product
-                    2. Purchase product
-                    3. View bill''')
-            user_option_choice = input()
-            if user_option_choice == '1':
-                view_product()
-
-            elif user_option_choice == '2':
-                pass
-            elif user_option_choice == '3':
-                pass
-            else:
-                print('Invalid options, please choose correct option')
-                login()
-
-        elif usertype == 'seller':
-            print('''
-                    Enter the option from below :
-                    1. Add product
-                    2. View your product
-                    3. View your product bill''')
-            user_option_choice = input()
-            if user_option_choice == '1':
-                add_product(username)
-            elif user_option_choice == '2':
-                view_your_product(username)
-
+        usertypeFun(usertype=usertype, username=username)
+    
     else:
         print('Invalid credentials')
 
 
-user_choice = input('Do you want to login or register? ').lower().strip()
+def main():
+    user_choice = input('Do you want to login or register? ').lower().strip()
 
-if user_choice == 'register':
-    register()
-elif user_choice == 'login':
-    login()
+    if user_choice == 'register':
+        register()
+    elif user_choice == 'login':
+        login()
+    else:
+        print('Invalid choice. Please try again.')
+
+if __name__ == "__main__":
+    main()
